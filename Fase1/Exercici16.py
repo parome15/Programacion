70275eab6013ece3d4a4a02b464dbee2f679c98a
romeromesquidapau@gmail.com <romeromesquidapau@gmail.com>
2017-01-16 16:17:51 +0100

    ejercicio 16

---
+++ Fase1/Exercici16.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+'''Escribe un programa que escriba en pantalla los 30 primeros números naturales (del 1 al 30), 
+así como su media aritmética.'''
+
+for numero in range(1,31):
+	print (numero)
