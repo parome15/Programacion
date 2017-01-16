197abb6dbceaa660c53dda83f6f010c12370ede3
romeromesquidapau@gmail.com <romeromesquidapau@gmail.com>
2017-01-16 16:26:53 +0100

    ejercicio 18

---
+++ Fase1/Exercici18.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+
+'''Escribe un programa que visualice los n-primeros múltiplos de 2, 
+siendo n un valor leído por teclado.'''
+
+numeroN = int(input('Escribe el valor de n:'))
+
+nPrimerosNumeros = 2**numeroN
