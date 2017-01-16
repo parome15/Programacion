4ca925ac8daf591aa7b2d2b6fc400e5422114bec
romeromesquidapau@gmail.com <romeromesquidapau@gmail.com>
2017-01-16 16:02:58 +0100

    ejercicio 15

---
+++ Fase1/Exercici15.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+'''Escribe un programa que escriba cinco veces el mensaje estamos estudiando metodología 
+de la programación.'''
+
+for numero in range(0,5):
+	print ('Estamos estudiando metodología de la programación')
